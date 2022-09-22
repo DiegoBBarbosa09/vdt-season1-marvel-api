9b653349592fe4224e39020953ecdfabb9db7e94
diegobrito.b09 <diegobrito.b09@gmail.com>
2022-09-22 14:28:55 -0300

    Validacao dos Status Code POST

--- cypress/integration/post.spec.js
+++ cypress/integration/post.spec.js
@@ -1,43 +1,55 @@
 
 
 
-describe('POST /characters', function() {
+describe('POST /characters', function () {
 
-    before(function(){
-        cy.request({
-            method:'POST',
-            url:'/sessions',
-            body: {
-                email:'diegobrito@qacademy.io',
-                password: 'qa-cademy'
-            }
-        }).then(function(response){
-            expect(response.status).to.eql(200)
-            cy.log(response.body.token)
-            Cypress.env('token', response.body.token)
-        })
+    before(function () {
+        cy.back2ThePast()
+        cy.setToken()
     })
 
 
     it('deve cadastrar um persongem', function() {
-       
-       const charater = {
+
+        const character = {
             name: 'Wanda Maximorff',
             alias: 'Feiticeira Escarlate',
-            team: ['Vingadores' ],
+            team: ['Vingadores'],
             active: true
         }
 
-        cy.request({
-            method: 'POST',
-            url: '/characters',
-            body: charater,
-            headers: {
-                Authorization: Cypress.env('token')
-            }
-        }).then(function(response){
+       cy.postCharacter(character)
+            .then(function (response) {
             expect(response.status).to.eql(201)
+            cy.log(response.body.character_id)
+            expect(response.body.character_id.length).to.equal(24)
         })
 
+    })
+
+    context('quando o personagem já existe', function(){
+
+        const character = {
+            name: 'Pietro Maximorff',
+            alias: 'Mercurio',
+            team: ['Vingadores da costa oeste', 'Irmanda de mutantes'],
+            active: true
+        }
+
+        before(function(){
+            cy.postCharacter(character)
+            .then(function (response) {
+                expect(response.status).to.equal(201)
+            })            
+        })
+
+        it('não deve cadastar duplicado', function(){
+            cy.postCharacter(character)
+                .then(function (response) {
+                expect(response.status).to.equal(400)
+                expect(response.body.error).to.eql('Duplicate character')
+                
+            })
+        }) 
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,43 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('setToken', function () {
+    cy.api({
+        method: 'POST',
+        url: '/sessions',
+        body: {
+            email: 'diegobrito@qacademy.io',
+            password: 'qa-cademy'
+        },
+        failOnStatusCode: false
+    }).then(function (response) {
+        expect(response.status).to.eql(200)
+        cy.log(response.body.token)
+        Cypress.env('token', response.body.token)
+    })
+})
+
+Cypress.Commands.add('back2ThePast', function () {
+    cy.api({
+        method: 'DELETE',
+        url: '/back2thepast/632ba452cd900b001651db55',
+        failOnStatusCode: false
+    }).then(function (response) {
+        expect(response.status).to.eql(200)
+    })
+})
+
+Cypress.Commands.add('postCharacter', function(payload){
+    cy.api({
+        method: 'POST',
+        url: '/characters',
+        body: payload,
+        headers: {
+            Authorization: Cypress.env('token')
+        },
+        failOnStatusCode: false
+    }).then(function(response){
+        return response
+    })
+})
